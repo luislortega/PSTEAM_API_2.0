f71430a8bde5ffb895a9dece44b54e947260a073
Luis Leon <leon.luisgerardo775@gmail.com>
2019-11-29 16:59:04 -0600

    Database CLOUD

--- src/config/config.js
+++ src/config/config.js
@@ -3,14 +3,14 @@ const Sequelize = require('sequelize');
 const op = Sequelize.Op;
 
 module.exports = {
-  port: process.env.PORT || 8081,
+  port: process.env.PORT || 8082,
   db: {
-    database: process.env.DB_NAME || 'psteam',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || '1298Luis',
+    database: process.env.DB_NAME || 'd69gujn4lbr2gk',
+    user: process.env.DB_USER || 'nehvbemosvjsyy',
+    password: process.env.DB_PASS || 'b90839bb9c0c804546a9bdb3409f3fce33056db7f425221bdae526821bc1b1ed',
     options: {
       dialect: process.env.DIALECT || 'postgres',
-      host: process.env.HOST || 'localhost',
+      host: process.env.HOST || 'ec2-174-129-255-26.compute-1.amazonaws.com',
       operatorsAliases: op.gt, //Fix the deprecated string based operator
     },
   },
