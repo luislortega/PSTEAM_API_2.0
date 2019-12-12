a592618f6f9a5538c22e46ce51df044e6c00f372
luislortega <a18016323@alumnos.uady.mx>
2019-12-11 20:44:44 -0600

    Final commit

--- seed/index.js
+++ seed/index.js
@@ -21,39 +21,43 @@ var con = mysql.createConnection({
 });
 
 let array_usuarios = []
-
 getAllUsers().then(function () {
     sequelize.sync({ force: true })
         .then(async function () {
             await Promise.all(
                 array_usuarios.forEach(element => {
-                    usuario.create({
-                        usuario: element.usuario,
-                        senha: element.senha,
-                        validade: element.validade,
-                        host: element.host,
-                        vendedor: "SayGus",
-                        id_device: element.id,
-                        bootloader: element.bootloader,
-                        board: element.board,
-                        brand: element.brand,
-                        device: element.device,
-                        display: element.display,
-                        fingerprint: element.fingerprint,
-                        hardware: element.hardware,
-                        manufacturer: element.manufacturer,
-                        host: element.host,
-                        model: element.model
-                    }
+                    
+                        usuario.create({
+                            usuario: element.usuario,
+                            senha: element.senha,
+                            validade: element.validade,
+                            host: element.host,
+                            vendedor: element.vendedor,
+                            id_device: element.id,
+                            bootloader: element.bootloader,
+                            board: element.board,
+                            brand: element.brand,
+                            device: element.device,
+                            display: element.display,
+                            fingerprint: element.fingerprint,
+                            hardware: element.hardware,
+                            manufacturer: element.manufacturer,
+                            host: element.host,
+                            model: element.model
+                        }
 
-                    )
-                    console.log("hola")
+                        )
+
+
                 })
             )
         })
 })
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 function getAllUsers() {
+    console.log("Sistema de migracion mysql a postgres")
     return new Promise(function (resolve, reject) {
         con.connect(function (err) {
             if (err) throw err;
@@ -61,13 +65,14 @@ function getAllUsers() {
                 if (err) throw err;
                 result.forEach(element => {
                     array_usuarios.push(element)
-                    console.log("hola")
 
                 });
+                console.log("Empezando a migrar datos...")
+
                 resolve();
             });
         })
-    })  
+    })
 }
 
 
